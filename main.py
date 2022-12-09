cb7430047647f6d24296021c4cd31e28f8bc20fe
DEVELoper <syabblogger@gmail.com>
2022-12-09 19:27:42 +0500

    updated with Display Panel and Login Information

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 print("Student Management System using Python language")
 
 class student:
-    def __init__(self, name, rollNumber):
-        self.name = name
-        self.rollNumber = rollNumber
+    def displayInfo(self, name, rollNumber):
+        print("Your Name is: ",name)
+        print("Your Roll Number is: ",rollNumber)
 
-    def displayInfo(self):
-        print("UserName: ", self.name)
-        print("RollNumber: ", self.rollNumber)
-        
+class DisplayPanel(student):
+    def loginForm(self):
+        name = str(input("Enter Your Key: "))
+        pinCode = int(input("Enter Your Pin Code: "))
+        if pinCode == 123321:
+            self.displayInfo(name, pinCode)
+        else:
+            print("Invalid PIN Code or Key Phrase ")
+
+
+panel = DisplayPanel()
+panel.loginForm()
